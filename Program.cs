d2641ea83b61acef7aab210aab67348d6f104aec
dragonalias <dragon1600@gmail.com>
2022-02-11 14:31:09 +0100

    Feat: Clean project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment5__RPG_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
