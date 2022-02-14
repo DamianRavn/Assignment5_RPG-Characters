f1f07073d769054d11e693d7ec63012d95310f50
dragonalias <dragon1600@gmail.com>
2022-02-14 21:38:18 +0100

    Feat: Added testing skeleton WIP

---
+++ Assignment5_RPG-Characters_Tests/HeroTests.cs
@@ -0,0 +1,103 @@
+﻿using Assignment5__RPG_Characters;
+using System;
+using Xunit;
+
+namespace Assignment5_RPG_Characters_Tests
+{
+    public class HeroTests
+    {
+        //Level tests
+        [Fact]
+        public void CharacterCreation_Level1_CorrectLevelOnCreation()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void LevelUp_Level2_HaveCorrectAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        //Attribute tests
+        [Fact]
+        public void MageAttributes_DefaultAttributes_HaveCorrectAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void RogueAttributes_DefaultAttributes_HaveCorrectAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void RangerAttributes_DefaultAttributes_HaveCorrectAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void WarriorAttributes_DefaultAttributes_HaveCorrectAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void MageAttributes_LevelUpAttributes_HaveIncreasedAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void RogueAttributes_LevelUpAttributes_HaveIncreasedAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void RangerAttributes_LevelUpAttributes_HaveIncreasedAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void WarriorAttributes_LevelUpAttributes_HaveIncreasedAttributes()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ Assignment5_RPG-Characters_Tests/ItemTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+using Assignment5__RPG_Characters;
+
+namespace Assignment5_RPG_Characters_Tests
+{
+    public class ItemTests
+    {
+    }
+}
