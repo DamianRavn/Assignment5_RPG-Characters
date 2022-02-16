2c74cb20f4caeded456151bb931a137dc2e2869b
dragonalias <dragon1600@gmail.com>
2022-02-16 17:33:27 +0100

    Fix: Made the interfaces public and added the needed methods to iitem

--- Assignment5_RPG-Characters/src/Interfaces.cs
+++ Assignment5_RPG-Characters/src/Interfaces.cs
@@ -1,15 +1,17 @@
-﻿
+﻿using System;
 
 namespace Assignment5__RPG_Characters
 {
     /// <summary>
-    /// To be an item, you simply have to have the item component. Composition, opposed to inheritance
+    /// To be an item, you have to have the item component. Composition, opposed to inheritance.
     /// </summary>
-    interface IItem
+    public interface IItem
     {
         public ItemComponent ItemComponent { get; set; }
 
-        public void Equip();
+        public string GetItemType();
+        public Exception ItemException();
+        //public T GetAttributes<T>();
     }
     /// <summary>
     /// Weapon interface uses the item interface, because then every object that uses the weapon interface is forced to use the item interface aswell (Since weapon is an item, this makes sense)
