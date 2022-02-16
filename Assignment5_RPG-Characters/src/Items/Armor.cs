ba7175b0ed1d176860b68bf979dd1c7b954d5c2c
dragonalias <dragon1600@gmail.com>
2022-02-16 17:34:18 +0100

    Feat: implemented the Armor and Weapon component with the needed interface methods and constructor.

--- Assignment5_RPG-Characters/src/Items/Armor.cs
+++ Assignment5_RPG-Characters/src/Items/Armor.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
 
 namespace Assignment5__RPG_Characters
 {
+    /// <summary>
+    /// An armor is used to boost primary attributes. Implemets the IAmor interface
+    /// </summary>
     public class Armor : IArmor
     {
+        public ArmorComponent ArmorComponent { get; set; }
+        public ItemComponent ItemComponent { get; set; }
+
+        public Armor(string name, int level, ItemSlot itemSlot, ArmorType type, PrimaryAttributes attributes)
+        {
+            ArmorComponent = new ArmorComponent(type, attributes);
+            ItemComponent = new ItemComponent(name, level, itemSlot);
+        }
+        /// <summary>
+        /// What type of amor is it?
+        /// </summary>
+        /// <returns>Armor type as a string</returns>
+        public string GetItemType()
+        {
+            return ArmorComponent.ArmorType.ToString();
+        }
+
+        /// <summary>
+        /// Returns the generic exception for this item
+        /// </summary>
+        /// <returns>InvalidArmorException</returns>
+        public Exception ItemException()
+        {
+            return new InvalidArmorException();
+        }
     }
 }
--- Assignment5_RPG-Characters/src/Items/Weapon.cs
+++ Assignment5_RPG-Characters/src/Items/Weapon.cs
@@ -1,7 +1,44 @@
 ﻿
+using System;
+
 namespace Assignment5__RPG_Characters
 {
+    /// <summary>
+    /// A weapon is used to deal more damage. Implements the IWeapon interface.
+    /// </summary>
     public class Weapon : IWeapon
     {
+        public WeaponComponent WeaponComponent { get; set; }
+        public ItemComponent ItemComponent { get; set; }
+
+
+        public Weapon(string name, int level, ItemSlot itemSlot,  WeaponType type, int baseDamage, float attackSpeed)
+        {
+            WeaponComponent = new WeaponComponent(type, baseDamage, attackSpeed);
+            ItemComponent = new ItemComponent(name, level, itemSlot);
+        }
+
+        //public T GetAttributes<T>()
+        //{
+        //    return (T)(object)WeaponComponent.WeaponAttributes;
+        //}
+
+        /// <summary>
+        /// What type of weapon is it?
+        /// </summary>
+        /// <returns>Weapon type as a string</returns>
+        public string GetItemType()
+        {
+            return WeaponComponent.WeaponType.ToString();
+        }
+
+        /// <summary>
+        /// Returns the generic exception for this item
+        /// </summary>
+        /// <returns>InvalidWeaponException</returns>
+        public Exception ItemException()
+        {
+            return new InvalidWeaponException();
+        }
     }
 }
