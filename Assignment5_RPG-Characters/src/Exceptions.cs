e090c46d3545b9d84673793615ed25f95298bf6a
dragonalias <dragon1600@gmail.com>
2022-02-16 17:29:33 +0100

    Feat: Added custom exceptions

---
+++ Assignment5_RPG-Characters/src/Exceptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Assignment5__RPG_Characters
+{
+    [Serializable]
+    public class InvalidWeaponException : Exception
+    {
+        public InvalidWeaponException()
+        {
+        }
+
+        public InvalidWeaponException(string message) : base(message)
+        {
+        }
+
+        public InvalidWeaponException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidWeaponException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    public class InvalidArmorException : Exception
+    {
+        public InvalidArmorException()
+        {
+        }
+
+        public InvalidArmorException(string message) : base(message)
+        {
+        }
+
+        public InvalidArmorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidArmorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
