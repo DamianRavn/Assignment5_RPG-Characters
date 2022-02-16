2c9aef2829821849b7bcfdebe32aa5000b3539f9
dragonalias <dragon1600@gmail.com>
2022-02-16 22:44:23 +0100

    Fix: weapons no longer require a slot since they should always be in the weapon slot

--- Assignment5_RPG-Characters/Program.cs
+++ Assignment5_RPG-Characters/Program.cs
@@ -9,7 +9,7 @@ namespace Assignment5__RPG_Characters
             Mage Gandalf = new Mage("Gandalf the White");
             Gandalf.LevelUp();
             Gandalf.LevelUp();
-            Gandalf.Equip(new Weapon("White Staff", 2, ItemSlot.WEAPON, WeaponType.STAFF, 14, 1.0f));
+            Gandalf.Equip(new Weapon("White Staff", 2, WeaponType.STAFF, 14, 1.0f));
             Gandalf.Equip(new Armor("White Robes", 2, ItemSlot.BODY, ArmorType.CLOTH, new PrimaryAttributes(0, 0, 7)));
 
             Console.WriteLine(Gandalf.ToString());
